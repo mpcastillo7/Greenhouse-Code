e0b23d49dd17d942db828645ca9c1a43b40652b1
Matt Castillo <mpcastillo7@outlook.com>
2018-12-28 16:23:28 -0800

    added doctest

---
+++ test.py
@@ -0,0 +1,10 @@
+"""some test functions for stuff"""
+
+def splice(lst1, lst2):
+    """
+    combines two lists into one list of ordered pairs
+    """
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
