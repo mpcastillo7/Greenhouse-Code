58a23a159d0df4bc3ec336bd64b26875884ecd43
Matt Castillo <mpcastillo7@outlook.com>
2018-12-28 17:03:17 -0800

    added splice function

--- test.py
+++ test.py
@@ -3,7 +3,31 @@
 def splice(lst1, lst2):
     """
     combines two lists into one list of ordered pairs
+    >>> lst1 = [1, 1, 1, 1]
+    >>> lst2 = [2, 2, 2, 2]
+    >>> spliced = splice(lst1, lst2)
+    >>> spliced
+    [(1, 2), (1, 2), (1, 2), (1, 2)]
+    >>> spliced2 = splice(lst2, lst1)
+    >>> spliced2
+    [(2, 1), (2, 1), (2, 1), (2, 1)]
+
+    >>> lst1 = [1, 1]
+    >>> lst2 = [1]
+    >>> splice(lst1, lst2)
+    Traceback (most recent call last):
+    ...
+    ValueError: Lists are not the same size
     """
+    if len(lst1) != len(lst2):
+        raise ValueError("Lists are not the same size")
+    return list(zip(lst1, lst2))
+
+def averagedList(lst):
+    for i in range(0, len(lst)-1):
+        lst[i] = 
+
+
 
 if __name__ == "__main__":
     import doctest
