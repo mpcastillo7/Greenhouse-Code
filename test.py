8f194eb961292dd7e3508418e24f650d02b2f3a1
Matt Castillo <mpcastillo7@outlook.com>
2018-12-30 13:19:48 -0800

    hardware + libraries

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 """
-test code for the DHT22 (Temperature/Humidity Sensor)
-
+Test code for the DHT22 (Temperature/Humidity Sensor)
 """
+
+#libaries
+import AdafruitDHT
+from matplotlib import pyplot as plt
+from gpiozero import LED
+
+#hardware
+sensor = AdafruitDHT.DHT22
