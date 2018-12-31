9ed779a625abf039f547f5f5144f953ec95fa990
Matt Castillo <mpcastillo75@gmail.com>
2018-12-30 23:00:07 -0800

    changed label and what plots

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ pin = 4
 led.on() #indicates things good
 
 #graph settings
-graph.xlabel("Number of 15s Intervals")
+graph.xlabel("Number of Intervals")
 graph.ylabel("Humidity Values (%)")
 
 #read DHT sensor for 2 minutes, 6 times a minute (every 10s) + graph data
@@ -22,4 +22,4 @@ results = readDHT(2, pin, frequency=6)
 time = results[2]
 temperature = results[0]
 humidity = results[1]
-valuePlot(temperature, time, graph)
+valuePlot(humidity, time, graph)
