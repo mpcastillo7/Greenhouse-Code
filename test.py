27dfebf301d00739d90fffaab02b67e4c46e840d
Matt Castillo <mpcastillo7@outlook.com>
2018-12-30 16:07:32 -0800

    added hardware settings

--- test.py
+++ test.py
@@ -3,9 +3,25 @@ Test code for the DHT22 (Temperature/Humidity Sensor)
 """
 
 #libaries
-import AdafruitDHT
+import Adafruit_DHT
 from matplotlib import pyplot as plt
 from gpiozero import LED
+import time
+import functions
 
-#hardware
-sensor = AdafruitDHT.DHT22
+#hardware configuration
+led = LED(22)
+sensor = Adafruit_DHT.DHT22
+pin = 4
+led.on()
+
+#graph settings
+plt.xlabel("Number of 15s Intervals")
+plt.ylabel("Humidity Values (%)")
+
+def hreadFor(mins, humidityVals=[], tempVals=[], t=[]):
+    minutes = 4*mins
+    return humidityVals, tempVals, t
+
+
+read = AdafruitDHT.read_retry
